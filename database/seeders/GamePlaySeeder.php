8659ecd7d9a7204d43cdd6bfebfda87a39cf25fc
petrelli07 <osita.chiemeke@gmail.com>
2021-01-22 17:54:40 +0100

    fixed

--- database/seeders/GamePlaySeeder.php
+++ database/seeders/GamePlaySeeder.php
@@ -20,7 +20,7 @@ class GamePlaySeeder extends Seeder
     public function getRandomUser(){
     
         
-        $randomId = rand(200,100000);
+        $randomId = rand(1,10000);
 
 
         //$playerGameData = Player::find($randomUserId)->games()->get();
